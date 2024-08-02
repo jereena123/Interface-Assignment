758c7097484bff092865b811db0ae8aa406c241f
jereena123 <132034967+jereena123@users.noreply.github.com>
2024-08-01 21:34:31 -0700

    Add files via upload

---
+++ MyAnimals.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Q2_Interface
+{
+    public class MyAnimals : Animals, IMammals
+    {
+        private int _bodyTemp;
+
+        // Property BodyTemp with get/set accessors
+        public int BodyTemp
+        {
+            get => _bodyTemp;
+            set => _bodyTemp = value;
+        }
+
+        public int GetBodyTemp(string _mammals)
+        {
+            return _bodyTemp;
+        }
+
+        // Implement the abstract method from Animals
+        public override void SaySomething()
+        {
+            Console.WriteLine("I am an animal.");
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Q2_Interface
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Create an instance of MyAnimals
+            Console.WriteLine("Enter a mammal type:");
+            string mammalType = Console.ReadLine(); // Accept user input for mammal type (not used in this example)
+
+            MyAnimals myAnimal = new MyAnimals
+            {
+                BodyTemp = 37 // Set some value for BodyTemp
+            };
+
+            // Call the SaySomething method
+            myAnimal.SaySomething();
+
+            // Call the GetBodyTemp method and display the result
+            // Passing the required string parameter
+            int temp = myAnimal.GetBodyTemp(mammalType);
+            Console.WriteLine($"Body Temperature: {temp}");
+
+            // Keep the console window open
+            Console.ReadKey();
+        }
+
+    }
+
+}
